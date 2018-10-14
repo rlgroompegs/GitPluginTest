f1bdcb2ee454ea2480912b9c07ad49069888df6d
Randy Groom <randy.groom@pegasus.io>
2018-10-14 04:14:49 -0700

    Added main method

--- src/packt/book/jee_eclipse/gitplugintest/PluginTestClass.java
+++ src/packt/book/jee_eclipse/gitplugintest/PluginTestClass.java
@@ -1,7 +1,11 @@
 package packt.book.jee_eclipse.gitplugintest;
 
 public class PluginTestClass {
-  public void NewMethod() {
+	public static void main(String[] args) { 
+	    System.out.println("Hello Git, from branch bug#1234 !!"); 
+	  }
+
+  private void NewMethod() {
 	  // Do Something
   }
 }
